88d6b0bb2bd128c8adffba27555783b804995814
egebasturk <alp.ege.basturk@gmail.com>
2018-10-14 19:25:28 +0300

    Empty values were filled and data type was changed to float64

--- Code/process_data.py
+++ Code/process_data.py
@@ -1,4 +1,5 @@
 import pandas as pa
+import numpy as np
 
 raw_data = pa.read_csv("../Data/telco-customer-churn/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
@@ -22,7 +23,11 @@ Electronic check = 1
 Credit card = 2
 Bank transfer = 3
 '''
-raw_data.replace(('Yes', 'No', 'Male', 'Female', 'No phone service', 'DSL', 'Fiber optic', 'No internet service', 'Month-to-month', 'One year', 'Two year', 'Mailed check', 'Electronic check', 'Credit card (automatic)', 'Bank transfer (automatic)')
-                 , (1,    0,     1,       0,               2,          1,       2,              2,                        0,            1,            2,          0,             1,                   2,             3), inplace=True)
-print(raw_data)
+raw_data.replace(('Yes', 'No', 'Male', 'Female', 'No phone service', 'DSL', 'Fiber optic', 'No internet service', 'Month-to-month', 'One year', 'Two year', 'Mailed check', 'Electronic check', 'Credit card (automatic)', 'Bank transfer (automatic)', ' ')
+                 , (1,    0,     1,       0,               2,          1,       2,              2,                        0,            1,            2,          0,             1,                   2,             3,                                  0), inplace=True)
+#print(raw_data)
+raw_data[['TotalCharges']] = raw_data[['TotalCharges']].apply(pa.to_numeric)
+for column in range(0,21):
+    print(raw_data.iloc[:, column])
+
 pa.DataFrame.to_csv(raw_data, "../Data/telco-customer-churn/WA_Fn-UseC_-Telco-Customer-Churn_Processed.csv")
