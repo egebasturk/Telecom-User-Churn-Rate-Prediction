0617a34b3ec857b1015a40af0ef19118fc24c412
egebasturk <alp.ege.basturk@gmail.com>
2018-11-04 01:16:12 +0300

    Readability improvements

--- Code/nn_prototype_keras.py
+++ Code/nn_prototype_keras.py
@@ -9,15 +9,18 @@ from keras.layers import Dense
 import pandas as pa
 import math
 
+# This reads data from the csv and save it to a data frame object
 data = pa.read_csv("../Data/telco-customer-churn/WA_Fn-UseC_-Telco-Customer-Churn_Processed.csv")
 #print(data)
+# following separate features and the labels
 features = data.iloc[:, 2:21].copy()
 features = np.squeeze(np.asarray(features))
 #print(features)
-target_Column = data.iloc[:, 21].copy()
+label_Column = data.iloc[:, 21].copy()
 #print(features) #-DEBUG
 
 # Model creation
+# tells keras to create the model
 model = Sequential()
 model.add(Dense(20, input_dim=19, activation='relu'))
 model.add(Dense(5, activation='relu'))
@@ -30,16 +33,18 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 data_div_ratio = 0.60 # 60% training 40% test
 rows = 7043
 training_rows = math.floor(rows * data_div_ratio)
-model.fit(features[training_rows:], target_Column[training_rows:], epochs=100, batch_size=5)
+model.fit(features[training_rows:], label_Column[training_rows:], epochs=100, batch_size=5)
 
 # Evaluate the model
-scores = model.evaluate(features[:training_rows], target_Column[:training_rows])
+scores = model.evaluate(features[:training_rows], label_Column[:training_rows])
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # Predict
 predictions = model.predict(features)
 # Round predictions
 predictions = [round(x[0]) for x in predictions]
-mat ={'Real': np.squeeze(np.asarray(target_Column)), 'Predicted': np.squeeze(np.asarray(predictions))}
+
+# Saves prediction to a new csv for manual control
+mat ={'Real': np.squeeze(np.asarray(label_Column)), 'Predicted': np.squeeze(np.asarray(predictions))}
 dataFrame = pa.DataFrame(data=mat)
 dataFrame.to_csv("../Data/telco-customer-churn/Predictions.csv")
