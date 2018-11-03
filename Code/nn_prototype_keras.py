8c9eec0f7f88ef9a77152603ad15806440b4fd24
egebasturk <alp.ege.basturk@gmail.com>
2018-11-04 02:18:30 +0300

    Data split which was done manually was converted to sklearn functions. Also feature scaling was applied.

--- Code/nn_prototype_keras.py
+++ Code/nn_prototype_keras.py
@@ -8,6 +8,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 import pandas as pa
 import math
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
 
 # This reads data from the csv and save it to a data frame object
 data = pa.read_csv("../Data/telco-customer-churn/WA_Fn-UseC_-Telco-Customer-Churn_Processed.csv")
@@ -17,7 +20,16 @@ features = data.iloc[:, 2:21].copy()
 features = np.squeeze(np.asarray(features))
 #print(features)
 label_Column = data.iloc[:, 21].copy()
-#print(features) #-DEBUG
+
+# Using Sklearn data split function
+features_train, features_test, label_train, label__test = train_test_split(features, label_Column, test_size=0.40)
+
+# Feature scaling. Needed since features vary currently a lot
+scaler = StandardScaler()
+scaler.fit(features_train)
+
+features_train = scaler.transform(features_train)
+features_test = scaler.transform(features_test)
 
 # Model creation
 # tells keras to create the model
@@ -30,21 +42,21 @@ model.add(Dense(1,activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Model fitting
-data_div_ratio = 0.60 # 60% training 40% test
-rows = 7043
-training_rows = math.floor(rows * data_div_ratio)
-model.fit(features[training_rows:], label_Column[training_rows:], epochs=100, batch_size=5)
+model.fit(features_train, label_train, epochs=100, batch_size=5)
 
 # Evaluate the model
-scores = model.evaluate(features[:training_rows], label_Column[:training_rows])
+scores = model.evaluate(features_test, label__test)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # Predict
-predictions = model.predict(features)
+predictions = model.predict(features_test)
 # Round predictions
 predictions = [round(x[0]) for x in predictions]
 
 # Saves prediction to a new csv for manual control
-mat ={'Real': np.squeeze(np.asarray(label_Column)), 'Predicted': np.squeeze(np.asarray(predictions))}
+mat ={'Real': np.squeeze(np.asarray(label__test)), 'Predicted': np.squeeze(np.asarray(predictions))}
 dataFrame = pa.DataFrame(data=mat)
 dataFrame.to_csv("../Data/telco-customer-churn/Predictions.csv")
+
+print(confusion_matrix(label__test, predictions))
+print(classification_report(label__test, predictions))
